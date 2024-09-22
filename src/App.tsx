3f0bd31d7deddf8146c4034e961d30d23b3b2262
keralex <kerlis.aguado26@gmail.com>
2024-09-22 17:17:10 -0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import DashBoard from "./components/pages/Dashboard/Dashboard"
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
-
   return (
-    <DashBoard />
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<DashBoard />} />
+      </Routes>
+    </Router>
+  );
 }
+
 
 export default App
