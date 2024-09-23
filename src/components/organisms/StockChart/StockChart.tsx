efed8eb07d0dd8d52635aac8e903b655b0765378
keralex <kerlis.aguado26@gmail.com>
2024-09-23 20:40:28 -0300

    add interval select

---
+++ src/components/organisms/StockChart/StockChart.tsx
@@ -0,0 +1,52 @@
+import { Box } from "@mui/material";
+import { StockValue } from "../../../types/StocksTypes"
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from "highcharts-react-official";
+import { useRef } from "react";
+
+interface StockChartProps {
+    data: StockValue[];
+    symbol: string;
+}
+
+const StockChart: React.FC<StockChartProps> = ({ data, symbol }) => {
+
+    const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+    const options: Highcharts.Options = {
+
+        navigator: {
+            enabled: false,
+        },
+        xAxis: {
+            type: 'datetime',
+            minTickInterval: 60 * 1000,
+        },
+
+        title: {
+            text: `${symbol} Stock Price`
+        },
+        series: [{
+            type: 'candlestick',
+            name: `${symbol} Stock Price`,
+            data: data?.map((value: StockValue) => [
+                Date.parse(value.datetime),
+                parseFloat(value.open),
+                parseFloat(value.high),
+                parseFloat(value.low),
+                parseFloat(value.close),
+            ])
+        }],
+    };
+
+    return (
+        <Box>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+                ref={chartComponentRef}
+            />
+        </Box>
+    )
+}
+
+export default StockChart;
--- src/components/pages/StockDetail/StockDetail.tsx
+++ src/components/pages/StockDetail/StockDetail.tsx
@@ -1,58 +1,68 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import PageLayout from "../../templates/PageLayout";
-import Highcharts from "highcharts/highstock";
-import HighchartsReact from 'highcharts-react-official';
 import { useParams } from "react-router-dom";
+import dayjs, { Dayjs } from 'dayjs';
+
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import useFetchStockDetail from "../../../hooks/useFetchStocksDetail";
-import { StockValue } from "../../../types/StocksTypes";
+import StockChart from "../../organisms/StockChart";
+import { Checkbox, FormControl, FormControlLabel, InputLabel, MenuItem, Select } from "@mui/material";
 
 
 
 const StockDetail: React.FC = () => {
-    const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+    const dateFormat = 'YYYY-MM-DD HH:mm:ss';
     const params = useParams()
-
-    console.log(params);
     const symbol = params.symbol;
-    console.log(symbol);
-    const { data, loading, error } = useFetchStockDetail({ symbol, interval: '1min', start_date: '2019-08-09 15:50:00' });
-    console.log(data);
 
-    const options: Highcharts.Options = {
-
-        navigator: {
-            enabled: false,
-        },
-        xAxis: {
-            type: 'datetime',
-            minTickInterval: 60 * 1000,
-        },
-
-        title: {
-            text: `${symbol} Stock Price`
-        },
-        series: [{
-            type: 'candlestick',
-            name: `${symbol} Stock Price`,
-            data: data?.values.map((value: StockValue) => [
-                Date.parse(value.datetime),
-                parseFloat(value.open),
-                parseFloat(value.high),
-                parseFloat(value.low),
-                parseFloat(value.close),
-            ])
-        }],
-    };
-
+    const [startDate, setStartDate] = useState<Dayjs | null>(dayjs('2022-04-17T15:30'));
+    const [endDate, setEndDate] = useState<Dayjs | null>(dayjs('2022-04-17T15:30'));
+    const [interval, setInterval] = useState<string>('1min');
+    const [realTime, setRealTime] = useState<boolean>(false);
+    const { data, loading, error } = useFetchStockDetail({ symbol, interval, start_date: startDate?.format(dateFormat), end_date: endDate?.format(dateFormat) });
 
     return (
         <PageLayout>
             <h1>Stock Detail</h1>
-            <HighchartsReact
-                highcharts={Highcharts}
-                options={options}
-                ref={chartComponentRef}
-            />
+            <form>
+                <FormControl>
+                    <FormControlLabel control={
+                        <Checkbox onChange={(event) => setRealTime(event.target.checked)} checked={realTime} />
+                    } label="Do you want the chart to update in real time?" />
+                </FormControl>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateTimePicker
+                        label="Start Date"
+                        value={startDate}
+                        onChange={(newValue: Dayjs | null) => setStartDate(newValue)}
+                        views={['year', 'month', 'day', 'hours', 'minutes', 'seconds']}
+                    />
+                    <DateTimePicker
+                        label="Start Date"
+                        value={endDate}
+                        onChange={(newValue: Dayjs | null) => setEndDate(newValue)}
+                        views={['year', 'month', 'day', 'hours', 'minutes', 'seconds']}
+                    />
+                </LocalizationProvider>
+                <FormControl fullWidth>
+                    <InputLabel id="interval-select-label">Interval</InputLabel>
+                    <Select
+                        id="interval-select"
+                        value={interval}
+                        label="Age"
+                        onChange={(event) => setInterval(event.target.value as string)}
+                    >
+                        <MenuItem value={'1min'}>1 Min</MenuItem>
+                        <MenuItem value={'5min'}>5 Min</MenuItem>
+                        <MenuItem value={'15min'}>15 Min</MenuItem>
+                    </Select>
+                </FormControl>
+            </form>
+            {symbol && data &&
+                <StockChart data={data.values} symbol={symbol} />
+            }
         </PageLayout>
     )
 };
