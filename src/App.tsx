8f213f610979c6ed1603ae0f9fbf2bb3d2f4c8a3
keralex <kerlis.aguado26@gmail.com>
2024-09-25 00:18:51 -0300

    move themeProvider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from '@mui/material/styles';
 import DashBoard from './components/pages/Dashboard/Dashboard';
 import StockDetail from './components/pages/StockDetail';
+import theme from './theme/theme.ts'
 
 const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<Routes>
-					<Route path='/' element={<DashBoard />} />
-					<Route path='/:symbol/:name' element={<StockDetail />} />
-				</Routes>
-			</BrowserRouter>
-		</QueryClientProvider>
+		<ThemeProvider theme={theme}>
+			<QueryClientProvider client={queryClient}>
+				<BrowserRouter>
+					<Routes>
+						<Route path='/' element={<DashBoard />} />
+						<Route path='/:symbol/:name' element={<StockDetail />} />
+					</Routes>
+				</BrowserRouter>
+			</QueryClientProvider>
+		</ThemeProvider>
+
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from '@mui/material/styles';
 import App from './App.tsx'
 import './index.css'
-import theme from './theme/theme.ts'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 )
